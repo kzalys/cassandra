5852f0e60dd84beae4b01a7761d6b7551002a272
Jaydeepkumar Chovatia <jchovati@uber.com>
2024-09-27 16:10:43 -0700

    Add a nodetool command to get the nodes going through repair

--- src/java/org/apache/cassandra/tools/NodeProbe.java
+++ src/java/org/apache/cassandra/tools/NodeProbe.java
@@ -2491,6 +2491,11 @@ public class NodeProbe implements AutoCloseable
     {
         autoRepairProxy.setRepairSessionTimeout(repairType, timeout);
     }
+
+    public Set<String> getOnGoingRepairHostIds(AutoRepairConfig.RepairType type)
+    {
+        return autoRepairProxy.getOnGoingRepairHostIds(type);
+    }
 }
 
 class ColumnFamilyStoreMBeanIterator implements Iterator<Map.Entry<String, ColumnFamilyStoreMBean>>
